082aca0e93dc31d061753e58e1b08e82e7adf22f
Drake Scott <dascott0204@gmail.com>
2017-03-06 00:58:47 -0600

    added order checks to make sure you cant sell more than you have or buy more than how much funds you have

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -12,19 +12,26 @@
                     <input type="number"
                            class="form-control"
                            placeholder="Quantity"
-                           v-model="quantity">
+                           v-model="quantity"
+                            :class="{danger: insufficientQuantity}">
                 </div>
                 <div class="pull-right">
                     <button class="btn btn-success"
                             @click="sellStock"
-                            :disabled="quantity <= 0"
-                    >Sell
+                            :disabled="insufficientQuantity || quantity <= 0"
+                    >{{ insufficientQuantity ? 'Not enough' : 'Sell' }}
                     </button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
 
 <script>
     import {mapActions} from 'vuex';
@@ -34,6 +41,11 @@
         data() {
             return {
                 quantity: 0
+            }
+        },
+        computed: {
+            insufficientQuantity() {
+                return this.quantity > this.stock.quantity;
             }
         },
         methods: {
--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -12,13 +12,14 @@
                     <input type="number"
                            class="form-control"
                            placeholder="Quantity"
-                           v-model="quantity">
+                           v-model="quantity"
+                            :class="{danger: insufficientFunds}">
                 </div>
                 <div class="pull-right">
                     <button class="btn btn-success"
                             @click="buyStock"
-                            :disabled="quantity <= 0"
-                    >Buy
+                            :disabled="insufficientFunds || quantity <= 0"
+                    >{{ insufficientFunds ? 'Insufficient Funds' : 'Buy' }}
                     </button>
                 </div>
             </div>
@@ -26,12 +27,26 @@
     </div>
 </template>
 
+<style scoped="">
+    .danger {
+        border: 1px solid red;
+    }
+</style>
+
 <script>
     export default {
         props: ['stock'],
         data() {
             return {
                 quantity: 0
+            }
+        },
+        computed: {
+            funds() {
+                return this.$store.getters.funds;
+            },
+            insufficientFunds() {
+                return this.quantity * this.stock.price > this.funds;
             }
         },
         methods: {
